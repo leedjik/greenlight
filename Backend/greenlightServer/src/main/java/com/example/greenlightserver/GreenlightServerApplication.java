b2e1975c136f9a42995d9839fded0891641cd926
이대직 <30641972+leedjik@users.noreply.github.com>
2022-07-10 18:40:21 +0900

    backend_ldj

---
+++ Backend/greenlightServer/src/main/java/com/example/greenlightserver/GreenlightServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.greenlightserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenlightServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenlightServerApplication.class, args);
+	}
+
+}
---
+++ Backend/greenlightServer/src/test/java/com/example/greenlightserver/GreenlightServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greenlightserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenlightServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
